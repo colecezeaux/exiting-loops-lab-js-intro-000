b679d87e20776d9eef8b8ba1de5368d6092d638d
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-07 17:19:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,12 +22,9 @@ return array
 
 function findBy(array, findFn) {
 for(var i = 0; i < array.length; i++) {
-var output;
-  if(array[i] === findFn) {
-    output = findFn
-  } else {
-    output = null
+  array.find(element === findFn)
+  if(array[i] !== findFn) {
+    return null
   }
 }
-return output
 }
