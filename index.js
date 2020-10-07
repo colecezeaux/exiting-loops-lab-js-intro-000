61851b3585d810952d5cff5321e04baf3f84ac7d
Cole Cezeaux <colecezeaux@gmail.com>
2020-10-07 18:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,12 +32,15 @@ return output
 
 function findBy(array, findFn) {
   var output = null
-for(var i = 0; i < array.length; i++) {
-  const index = array.indexOf(findFn)
-  if(index > -1) {
-    array.splice(index, 0)
-    output = array
+  const find = [] // constant variable itemName = empty array
+  for(var i = 0; i < array.length; i++){ // for loop; initialization is variable i = 0, condition is while var i is less than the length of the object cart, iteration is var i incrementing by 1 each loop
+    find.push(array[i].find) // each loop pushes the index of i’s itemName key value into the empty array of const itemName (giving us an array of only itemNames)
+    }
+// indexOf() syntax: array.indexOf(item, start)
+  const index = find.indexOf(findFn) // constant variable index = indexOf constant variable itemName, where the passed in string item is the item to search for
+  if(index > -1){ // [if cart.splice() method does not find the item, it will return -1] if statement; condition is while constant variable index is greater than -1 (and thereby has returned a result)
+     array.splice(index, 1) // splice object cart where index is constant variable index, and how many to remove is 1
+     output = array // updates variable output within the if statement to only change if the statement returns true to = updated cart object with removed element
   }
-}
-return output
+    return output // returns variable output; default or true if statement result
 }
